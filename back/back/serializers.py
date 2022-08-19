bc4e570271948eca12a198b2e8af5ce565d6d56f
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-08-19 15:57:58 +0300

    'update'

--- back/back/serializers.py
+++ back/back/serializers.py
@@ -20,6 +20,21 @@ class EtalonSerializer(serializers.ModelSerializer):
             EtalonProperty.objects.create(etalon=etalon, **etalon_property)
         return etalon
 
+    def update(self, instance, validated_data):
+        etalon_properties = validated_data.pop('etalon_properties')
+
+        for item in validated_data:
+            if Etalon._meta.get_field(item):
+                setattr(instance, item, validated_data[item])
+
+        EtalonProperty.objects.filter(etalon=instance).delete()
+
+        for etalon_property in etalon_properties:
+            EtalonProperty.objects.create(etalon=instance, **etalon_property)
+
+        instance.save()
+        return instance
+
     class Meta:
         model = Etalon
         fields = ('id','title', 'created_at', 'description', 'etalon_properties')
